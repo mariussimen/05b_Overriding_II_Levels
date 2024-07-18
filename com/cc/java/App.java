fdcd6660eb6e1828defef052c8ea55e16f7428c3
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-07-18 16:58:05 +0200

    Add files via upload

---
+++ com/cc/java/App.java
@@ -0,0 +1,26 @@
+package com.cc.java;
+
+public class App {
+    public static void main(String[] args) {
+    
+        Player player = new Player();
+
+        
+        // Variante 1
+        // for (int i = 0; i < 12; i++) {
+        //     output(player.play());  
+        // }
+
+        // Variante 2
+        // while (player.points < 13) {
+        //     output(player.play()); 
+        // }
+
+
+       
+    }
+
+    private static void output(String outStr) {
+        System.out.println(outStr);
+    }
+}
